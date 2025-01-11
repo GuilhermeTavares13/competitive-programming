e8a0549b428f769284f26a2590395c5ea076efcc
Guilherme Tavares <gui.tavares2002.contato@gmail.com>
2025-01-11 19:17:51 -0300

    Add exercise 13 - not finished yet

---
+++ leetcode/top-interview-150/13-roman-to-integer.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string.h>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        
+    }
+};
+
+
+int main() {
+    Solution solution;
+
+    cout << solution.romanToInt("III");
+    cout << solution.romanToInt("LVIII");
+    cout << solution.romanToInt("MCMXCIV");
+
+    return 0;
+}
