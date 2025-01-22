25a2ce32b703ce4e60aad9b7c30a34bc464e2840
Guilherme Tavares <gui.tavares2002.contato@gmail.com>
2025-01-22 12:51:10 -0300

    Exercise 392 completed

--- leetcode/top-interview-150/392-is-subsequence.cpp
+++ leetcode/top-interview-150/392-is-subsequence.cpp
@@ -5,17 +5,30 @@ using namespace std;
 
 
 class Solution {
-public:
+public:                 // aza      abzba   
     bool isSubsequence(string s, string t) {
-        for (int i = 0; i < t.length(); i++) {
-            
+        string subword;
+        int pos = 0;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = pos; j < t.length(); j++) {
+                if (s[i] == t[j]) {
+                    subword += t[j];
+                    pos = j + 1;
+                    break;
+                }
+            }
         }
+        
+        return s == subword;
     }
 };
 
 int main() {
     Solution solution;
-    cout << solution.isSubsequence("abc","ahbgdc") << endl; // Should return true
-    cout << solution.isSubsequence("axc","ahbgdc") << endl; // Should return false
+    // cout << solution.isSubsequence("abc","ahbgdc") << endl; // Should return true
+    // cout << solution.isSubsequence("axc","ahbgdc") << endl; // Should return false
+    // cout << solution.isSubsequence("acb","ahbgdc") << endl; // Should return false
+    // cout << solution.isSubsequence("bb","ahbgdc") << endl; // Should return false
+    cout << solution.isSubsequence("aza","abzba") << endl; // Should return false
     return 0;
 }
