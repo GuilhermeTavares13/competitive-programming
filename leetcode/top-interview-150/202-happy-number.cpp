6a047e131654d079628b1b8c575a46becd5a878c
Guilherme Tavares <gui.tavares2002.contato@gmail.com>
2025-01-23 13:10:49 -0300

    Update exercise 202

--- leetcode/top-interview-150/202-happy-number.cpp
+++ leetcode/top-interview-150/202-happy-number.cpp
@@ -10,21 +10,24 @@ public:
     bool isHappy(int n) {
         string num = to_string(n);
         int sum = 0;
-        for(int i = 0; i < num.length(); i++) {
-            
-            sum += num[i];
-            cout << num[i] << endl;
+        int c = 0;
+        while((sum != 1) && (c < 10)) {
+            sum = 0;
+            for(int i = 0; i < num.length(); i++) {
+                sum += pow((int) num[i] - 48,2); // ASCII numbers starts from 48
+            }
+            num = to_string(sum);
+            c++;
         }
-        
-        cout << sum << endl;
-
-        return false;
+        return sum == 1;
     }
 };
 
 int main() {
     Solution solution;
-    cout << solution.isHappy(19) << endl; // Should return true
-    // cout << solution.isHappy(2) << endl; // Should return false
+    // cout << solution.isHappy(19) << endl; // Should return true
+    cout << solution.isHappy(2) << endl; // Should return false
+    // cout << solution.isHappy(19) << endl; // Should return true
+    // cout << solution.isHappy(32) << endl; 
     return 0;
 }
