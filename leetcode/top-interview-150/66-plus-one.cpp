6fee6d7add45aa67ae5e24034bb20700b0baea09
Guilherme Tavares <gui.tavares2002.contato@gmail.com>
2025-01-29 12:52:36 -0300

    Add exercise 66 -  not completed yet

---
+++ leetcode/top-interview-150/66-plus-one.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string.h>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        
+    }
+};
+
+
+
+int main() {
+    Solution solution;
+
+    vector<int> test = {1,2,4};
+
+    solution.plusOne(test); // Should return [1,2,4]
+
+    test = {4,3,2,1};
+    solution.plusOne(test); // Should return [4,3,2,2]
+    
+    return 0;
+}
