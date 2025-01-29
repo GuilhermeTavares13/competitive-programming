f8927488d8469e06463030275867bb07a20ad223
Guilherme Tavares <gui.tavares2002.contato@gmail.com>
2025-01-28 22:08:58 -0300

    Add exercise 9

---
+++ leetcode/top-interview-150/palindrome-number.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string.h>
+#include <algorithm>
+#include <string>
+#include <cmath>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+
+    cout << solution.isPalindrome(121) << endl; // Should return true 
+
+    return 0;
+}
