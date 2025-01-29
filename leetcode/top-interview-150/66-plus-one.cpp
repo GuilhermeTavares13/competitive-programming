78fdc87eea9c26625c0a458d6e3cbcd2ff63ee01
Guilherme Tavares <gui.tavares2002.contato@gmail.com>
2025-01-29 13:02:06 -0300

    Update exercise

--- leetcode/top-interview-150/66-plus-one.cpp
+++ leetcode/top-interview-150/66-plus-one.cpp
@@ -10,7 +10,18 @@ using namespace std;
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
+        vector<int> number = digits;
+        for(int i = digits.size() - 1; i >= 0; i--) {
+            if (number[i] + 1 < 10) {
+                number[i] += 1; 
+            }
+        }
         
+        for(int i = 0; i < number.size(); i++){
+            cout << number[i] << endl;
+        }
+
+        return number;
     }
 };
 
@@ -21,10 +32,17 @@ int main() {
 
     vector<int> test = {1,2,4};
 
-    solution.plusOne(test); // Should return [1,2,4]
+    // solution.plusOne(test); // Should return [1,2,5]
 
-    test = {4,3,2,1};
-    solution.plusOne(test); // Should return [4,3,2,2]
-    
+    // test = {4,3,2,1};
+    // solution.plusOne(test); // Should return [4,3,2,2]
+
+    // test = {9};
+    solution.plusOne(test); // Should return [1,0]
+
+    // test = {4,3,2,9};
+    // solution.plusOne(test); // Should return [4,3,3,0]
+     
+
     return 0;
 }
